156b4e65eab779c3875c2a691bb2b0d6c84b086f
Christian Gutierrez <chrisarts.net@gmail.com>
2024-06-26 00:24:47 -0500

    Tetris Game reanimated

--- packages/tetris/src/board/components/BoardHeader.tsx
+++ packages/tetris/src/board/components/BoardHeader.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { useGameStatus } from "../../hooks/useGameStatus";
 
 interface BoardHeaderProps {
@@ -8,52 +8,47 @@ export const BoardHeader = ({ gameState }: BoardHeaderProps) => {
   const { score, level, rows } = gameState;
   const getItem = (label: string, value: string | number) => {
     return (
-      <View
-        style={{
-          paddingVertical: 15,
-          paddingHorizontal: 25,
-          borderRadius: 10,
-          borderColor: "white",
-          borderWidth: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          gap: 8,
-        }}
-      >
-        <Text
-          style={{
-            color: "white",
-            fontWeight: "bold",
-            fontFamily: "Digital-Bold",
-            fontSize: 16,
-          }}
-        >
-          {label}
-        </Text>
-        <Text
-          style={{
-            color: "white",
-            fontFamily: "Digital-Italic",
-            fontSize: 16,
-          }}
-        >
-          {value}
-        </Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>{label}</Text>
+        <Text style={styles.valueText}>{value}</Text>
       </View>
     );
   };
 
   return (
-    <View
-      style={{
-        flexDirection: "row",
-        justifyContent: "space-between",
-        marginBottom: 10,
-      }}
-    >
+    <View style={styles.content}>
       {getItem("SCORE", score)}
       {getItem("LINES", rows)}
       {getItem("LEVEL", level)}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+  container: {
+    paddingVertical: 15,
+    paddingHorizontal: 25,
+    borderRadius: 10,
+    borderColor: "white",
+    borderWidth: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 8,
+  },
+  title: {
+    color: "white",
+    fontWeight: "bold",
+    fontFamily: "Digital-Bold",
+    fontSize: 16,
+  },
+  valueText: {
+    color: "white",
+    fontFamily: "Digital-Italic",
+    fontSize: 16,
+  },
+});
