39fcef829cd73a0930178e6e3f3e44e802086842
Christian Gutierrez <chrisarts.net@gmail.com>
2025-01-15 23:56:39 -0500

    Sweep rows

--- packages/tetris/src/svg-app/programs/tetris.runtime.ts
+++
@@ -1,8 +0,0 @@
-import * as Layer from 'effect/Layer';
-import * as ManagedRuntime from 'effect/ManagedRuntime';
-
-export const TetrisMainLayer = TetrisServiceCtxLive.pipe(
-  Layer.provideMerge(GameStateCtxLive),
-);
-
-export const TetrisRuntime = ManagedRuntime.make(TetrisMainLayer);
